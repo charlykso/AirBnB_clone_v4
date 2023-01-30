309fb71047e9fccb702a09305170bd3e5cfece2a
Ikenna <ikenna@kali.Ikenna-PC>
2023-01-30 05:07:04 -0500

    API Status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -3,6 +3,15 @@
 let checked_box = {};
 $(document).ready(function () {
 	$('input:checkbox').change(function () {
+		// if ($(this).is(':checked')) {
+		// 	console.log($(this).data('name'));
+			
+		// }
+		// $('div.amenities h4').html(function () {
+		// 	Object.keys(checked_box).forEach(function (key) {
+		// 		amenities.push(checked_box[key]);
+		// 	});
+		// })
 		if ($(this).is(':checked')) {
 			checked_box[$(this).data('id')] = $(this).data('name');
 		}
@@ -21,4 +30,15 @@ $(document).ready(function () {
 		});
 		
 	});
+
+
+	$.get('http://0.0.0.0:5001/api/v1/status/',
+		function (data) {
+			console.log(data.status);
+			if (data.status === 'OK') {
+				$('DIV#api_status').addClass('available');
+			} else {
+				$('DIV#api_status').removeClass('available');
+			}
+		});
 });
