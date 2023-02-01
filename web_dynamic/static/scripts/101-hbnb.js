f03df7b395fc6558b748d291b5306965fd252159
Ikenna <ikenna@kali.Ikenna-PC>
2023-02-01 14:49:39 -0500

    Not complete Reviews

--- web_dynamic/static/scripts/101-hbnb.js
+++ web_dynamic/static/scripts/101-hbnb.js
@@ -3,7 +3,15 @@
 let checked_box = {};
 $(document).ready(function () {
 	$('input:checkbox').change(function () {
-		
+		// if ($(this).is(':checked')) {
+		// 	console.log($(this).data('name'));
+			
+		// }
+		// $('div.amenities h4').html(function () {
+		// 	Object.keys(checked_box).forEach(function (key) {
+		// 		amenities.push(checked_box[key]);
+		// 	});
+		// })
 		if ($(this).is(':checked')) {
 			checked_box[$(this).data('id')] = $(this).data('name');
 		}
